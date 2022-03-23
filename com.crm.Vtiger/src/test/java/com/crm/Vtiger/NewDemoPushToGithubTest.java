ffa3f03105dd71967ab510faf7a16ddb8711bc1e
gayat <gayat@LAPTOP-LDQFS899>
2022-03-23 17:38:50 +0530

    second repo

---
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/NewDemoPushToGithubTest.java
@@ -0,0 +1,12 @@
+package com.crm.Vtiger;
+
+import org.testng.annotations.Test;
+
+public class NewDemoPushToGithubTest
+{
+  @Test
+  public void push()
+  {
+	  System.out.println("moved to github successfully");
+  }
+}
