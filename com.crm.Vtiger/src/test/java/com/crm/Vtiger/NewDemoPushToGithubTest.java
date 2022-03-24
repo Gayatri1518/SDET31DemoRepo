58f64f773dc0093411b17ea27cf4ab3f72ef33a1
gayat <gayat@LAPTOP-LDQFS899>
2022-03-24 14:20:10 +0530

    user1 second repo

--- com.crm.Vtiger/src/test/java/com/crm/Vtiger/NewDemoPushToGithubTest.java
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/NewDemoPushToGithubTest.java
@@ -9,6 +9,6 @@ public class NewDemoPushToGithubTest
   {
 	  System.out.println("moved to github successfully");
 	  System.out.println("import user changes");
-	
+	  System.out.println("user modification");
   }
 }
