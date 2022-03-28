1d56f861f4d495bb0b4cbea47368991a8a7a3398
gayat <gayat@LAPTOP-LDQFS899>
2022-03-28 17:55:41 +0530

    commit

--- com.crm.Vtiger/src/test/java/com/crm/Vtiger/LoginTest.java
+++ com.crm.Vtiger/src/test/java/com/crm/Vtiger/LoginTest.java
@@ -22,34 +22,31 @@ public class LoginTest
     	String username=System.getProperty("username");
     	String password=System.getProperty("password");
     	
+    	
+    	WebDriver driver=null;
+    	if(browser.equalsIgnoreCase("chrome"))
+    	{
     	WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
-    	
-    	
-//    	WebDriver driver=null;
-//    	if(browser.equalsIgnoreCase("chrome"))
-//    	{
-//    	WebDriverManager.chromedriver().setup();
-//    	driver=new ChromeDriver();
-//    		//ChromeOptions chromeOptions= new ChromeOptions();
-//    		//chromeOptions.setBinary("C:\\Users\\Manish\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
-//    		// System.setProperty("webdriver.chrome.driver","C:\\Users\\manish\\Downloads\\chromedriver_win32\\chromedriver.exe");
-//    		// driver = new ChromeDriver(chromeOptions);
-//    	}
-//    	else if(browser.equals("firefox"))
-//    	{
-//    		WebDriverManager.firefoxdriver().setup();
-//    		driver=new FirefoxDriver();
-//    	}
-//    	else if(browser.equals("edge"))
-//    	{
-//    		WebDriverManager.edgedriver().setup();
-//    		driver=new EdgeDriver();
-//         }
-//    	else
-//    	{
-//    		System.out.println("specify valid browser");
-//    	}
+    	driver=new ChromeDriver();
+    		//ChromeOptions chromeOptions= new ChromeOptions();
+    		//chromeOptions.setBinary("C:\\selenium program\\chromedriver.exe");
+    		// System.setProperty("webdriver.chrome.driver","C:\\selenium program\\chromedriver.exe");
+    		 //driver = new ChromeDriver(chromeOptions);
+    	}
+    	else if(browser.equals("firefox"))
+    	{
+    		WebDriverManager.firefoxdriver().setup();
+    		driver=new FirefoxDriver();
+    	}
+    	else if(browser.equals("edge"))
+    	{
+    		WebDriverManager.edgedriver().setup();
+    		driver=new EdgeDriver();
+         }
+    	else
+    	{
+    		System.out.println("specify valid browser");
+    	}
     	
     	
     	driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
